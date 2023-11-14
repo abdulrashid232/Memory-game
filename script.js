20b55d328ad58eb89b055e76bcb6c8050ad04328
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2023-11-14 16:05:23 +0000

    corrected the tie result

--- script.js
+++ script.js
@@ -558,10 +558,10 @@ function createFlipCardGrid() {
     topNote.className = 'top-note';
   
     const winners = getWinners();
-    const winnerText = winners.length > 1 ? 'It\'s a tie' : 'Winner';
+    const winnerText = winners.length > 1 ? 'It\'s a tie' : 'Wins';
   
     const winner = document.createElement('h2');
-    winner.textContent = winners.length> 1 ? `${winnerText} `: `${winners.join(', ')}`;
+    winner.textContent = winners.length> 1 ? `${winnerText} `: `${winners.join(', ')} ${winnerText}`;
   
     const p = document.createElement('p');
     p.textContent = "Game Over! Here are the results...";
